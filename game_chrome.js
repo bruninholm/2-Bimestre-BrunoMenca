45dd15208e74470c627ff4d38035dbf7a18db88b
bruninholm <107251253+bruninholm@users.noreply.github.com>
2022-06-24 22:30:47 -0300

    Add files via upload

---
+++ game_chrome.js
@@ -0,0 +1,5 @@
+//CODIGO PARA HACKEAR GAME DO DINO CHROME
+Runner.instance_.playingIntro = true
+Runner.instance_.playingIntro = false
+Runner.instance_.gameOver = function(){}
+Runner.instance_.setSpeed (100)
